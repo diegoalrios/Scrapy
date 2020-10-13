8beb7602c27d3ecd0adabd8fd6212359ea643a57
Hindre <diego.al.rios96@gmail.com>
2020-10-13 14:06:37 -0500

    league scraper viola robots.txt :(

--- scrapypj/spiders/league_spider.py
+++ scrapypj/spiders/league_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+##ruta:
 
 class League_Spider(scrapy.Spider):
     ##vars
@@ -7,28 +7,42 @@ class League_Spider(scrapy.Spider):
     start_urls = [
         'https://www.leagueofgraphs.com/es/rankings/summoners/lan'
     ]
-    '''
-    ##de esta forma demos formato al comando scrapy crowl scrape
-    custom_settings = {
-        'FEED_URI' : 'autores.csv',
-        'FEED_FORMAT' : 'csv',
-        'CONCURRENT_REQUESTS':24
-        'FEED_EXPORT_ENCODING:'utf-8''
-    }   
-
-    '''
 
     ##methods
-    def parse_players(self, response, **kwargs):  ## **desempaquetar dict
-        pass
+    def parse_link_players(self, response, **kwargs):
+        print("2" * 100)
+        if kwargs:
+            list_players_link=kwargs['list_player_link']
+
+        list_players_link.extend(response.xpath('//div[contains(@class, "box-padding-10")]//table/tr/td/a[starts-with(@href, "/summoner")]/@href').getall())
+
+        list_button_link = response.xpath('//ul[@class="pagination"]/li/a[@href]/@href').getall()
+        list_view_button_link = response.xpath('//ul[@class="pagination"]/li/a[@href]/text()').getall()
+        next_buton = None
+
+        if len(list_button_link) == len(list_view_button_link):
+            for i in range(len(list_view_button_link)):
+                if list_view_button_link[i] == '>':
+                    next_buton = list_button_link[i]
+
+
+
+        if next_buton:
+            yield response.follow(next_buton, callback=self.parse_link_players,cb_kwargs={'list_player_link': list_players_link})
 
     def parse(self, response):
-        ## console: scrapy crawl scrape -o top.csv [.json etc]
-        pass
+       list_players_link=response.xpath(
+           '//div[contains(@class, "box-padding-10")]//table/tr/td/a[starts-with(@href, "/summoner")]/@href').getall()
+       list_button_link=response.xpath('//ul[@class="pagination"]/li/a[@href]/@href').getall()
+       list_view_button_link=response.xpath('//ul[@class="pagination"]/li/a[@href]/text()').getall()
+       next_buton=None
 
-''' 
-    ##crea un archivo html con la response
-    def parse(self,response):
-        with open('response.html', 'w', encoding='utf-8') as file:
-            file.write(response.text)
-'''
+       if len(list_button_link) == len(list_view_button_link):
+           for i in range(len(list_view_button_link)):
+               if list_view_button_link[i]=='>':
+                   next_buton=list_button_link[i]
+
+       if next_buton:
+           print(next_buton)
+           print("1"*100)
+           yield response.follow(next_buton, callback=self.parse_link_players, cb_kwargs={'list_player_link': list_players_link})
